7f3a1553941c7d1cc66cbd66d9503f40d458247e
Henrik Riskær <42350063+Byttemos@users.noreply.github.com>
2021-02-15 12:02:13 +0100

    hel

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 import sys, pygame
 from random import randint
-
+# test 
 
 # Set size of pygame window
 resolution = width, height = 800, 600
