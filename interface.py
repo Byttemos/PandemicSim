d49c960fede0b056009e697076783a348c4d3b8d
Laumanji <lau-ls@hotmail.com>
2021-05-13 16:31:54 +0200

    updated sliders

--- interface.py
+++ interface.py
@@ -14,19 +14,22 @@ welcomemsg = Label(root, text="Greetings from the PandemicSim dev team! \n Selec
 welcomemsg.grid(row=0, column=0)
 
 population_slider = Scale(root, from_=1, to=50000, orient=HORIZONTAL, label="Population:", length=210, resolution=200)
-population_slider.grid(row=2, column=0)
+population_slider.grid(row=1, column=0)
 
 masks_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of people with masks ( IN %):", length=220, resolution=5)
-masks_slider.grid(row=4, column=0)
+masks_slider.grid(row=2, column=0)
 
 vaccerSlider =Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of people vaccinated ( IN %):", length=220, resolution=5)
-vaccerSlider.grid(row=6, column=0)
+vaccerSlider.grid(row=3, column=0)
+
+testSlider =Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of test:", length=220, resolution=5)
+testSlider.grid(row=4, column=0)
 
 iteration_slider = Scale(root, from_=1, to=10000, orient=HORIZONTAL, label="Iterations:", length=210, resolution=10)
-iteration_slider.grid(row=8, column=0)
+iteration_slider.grid(row=5, column=0)
 
 runsim_button = Button(root, text=runString, bg="#82ff63", command=run)
-runsim_button.grid(row=9, column=0)
+runsim_button.grid(row=6, column=0)
 
 #Run loop of root window
 root.mainloop()
