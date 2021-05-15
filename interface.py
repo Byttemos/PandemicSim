9abf831fe5d62ad0e84799c64049e99cd9e13fe2
Laumanji <lau-ls@hotmail.com>
2021-05-15 13:45:39 +0200

    runsim parses more arguments

--- interface.py
+++ interface.py
@@ -5,7 +5,7 @@ import sys
 
 def run():
     runsim_button.configure(text="Running simulation...")
-    main.runSim(population_slider.get(), iteration_slider.get())
+    main.runSim(population_slider.get(), iteration_slider.get(), masks_slider.get(), vaccer_slider.get())
     runsim_button.configure(text="Simulation competed!")
     view_results_button.grid(row=7, column=0)
 
@@ -18,23 +18,20 @@ root = Tk()
 welcomemsg = Label(root, text="Greetings from the PandemicSim dev team! \n Select population size")
 welcomemsg.grid(row=0, column=0)
 
-population_slider = Scale(root, from_=0, to=50000, orient=HORIZONTAL, label="Population:", length=210, resolution=200)
+population_slider = Scale(root, from_=0, to=50000, orient=HORIZONTAL, label="Population:", length=220, resolution=200)
 population_slider.grid(row=1, column=0)
 
 masks_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of people with masks ( IN %):", length=220, resolution=5)
 masks_slider.grid(row=2, column=0)
 
-vaccerSlider =Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of people vaccinated ( IN %):", length=220, resolution=5)
-vaccerSlider.grid(row=3, column=0)
+vaccer_slider =Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of people vaccinated ( IN %):", length=220, resolution=5)
+vaccer_slider.grid(row=3, column=0)
 
-testSlider =Scale(root, from_=0, to=100, orient=HORIZONTAL, label="Number of test:", length=220, resolution=5)
-testSlider.grid(row=4, column=0)
-
-iteration_slider = Scale(root, from_=0, to=10000, orient=HORIZONTAL, label="Iterations:", length=210, resolution=10)
-iteration_slider.grid(row=5, column=0)
+iteration_slider = Scale(root, from_=0, to=10000, orient=HORIZONTAL, label="Iterations:", length=220, resolution=10)
+iteration_slider.grid(row=4, column=0)
 
 runsim_button = Button(root, text="Run Simulation", bg="#82ff63", command=run)
-runsim_button.grid(row=6, column=0)
+runsim_button.grid(row=5, column=0)
 
 view_results_button = Button(root, text="View Results", bg="#0096FF", command=view_results)
 
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from dataclasses import dataclass
 def callbacktest(nodesystem):
     #print(nodesystem.nodes)
     pass
-def runSim(n, iteration_number, log_steps = 10, callback = callbacktest):
+def runSim(n, iteration_number, mask_procent, vac_procent, log_steps = 10, callback = callbacktest):
 
     nodesys = ns(n)
 
