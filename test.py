1e24e217b86b39307d0b0fbbc7512ccd5a9098cd
Byttemos <hriskaer@gmail.com>
2021-05-16 14:37:32 +0200

    improved testing capabilities

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import main
 
 
-n = 200 #pop density
-iterations = 1000 #number of iterations
+n = 20000 #pop density
+iterations = 1 #number of iterations
 masks = 20 # percentage of mask-wearers
 vax = 10 #percentage of vaccinated nodes
-logsteps = 10 # log every nth frame
+logsteps = 1 # log every nth frame
 
 main.runSim(n, iterations, masks, vax, logsteps)
