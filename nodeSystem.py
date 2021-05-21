6bb7dc23e83494b3bbb162e1112af5a99544d72c
Byttemos <hriskaer@gmail.com>
2021-05-21 15:28:24 +0200

    nej

--- nodeSystem.py
+++ nodeSystem.py
@@ -8,50 +8,13 @@ class NodeSystem:
         self.nodes[:, [0, 1]] = np.random.randint([500, 500], size = (n, 2))
         self.nodes[:, [2, 3]] = np.random.randn(n, 2)
         self.nodes[:, 10] = np.random.randint(100, size = n)
-        self.healthy_color_mask = 0, 103, 0
-        self.healthy_color_no_mask = 0, 255, 0
-        self.sick_color_mask = 255, 100, 240
-        self.sick_color_no_mask = 150, 0 ,0
-        self.vaccinated = 0, 0, 255
-        self.immune = 127, 0, 255
-        self.dead = 0, 0, 0
         self.node_radius = 2
         self.mortality_rate = mortality_rate
         self.nodes[-1, 4] = 1 #create patient zero
         masks = (int(n/100))*int(mask_procent)
         self.nodes[[range(masks)], 5] = 1 #give people masks
-        self.infection_risk = 0.97
+        self.infection_risk = 0.50
 
-    def switch_state(self, row):
-        """4 = Healthy bool, 5 = Mask bool, 6 = Immune, 7 = Vaccinated, 8 = Dead"""
-        states = np.array([4,5,6,7,8])
-        find_states = np.where(nodes== 1)
-
-        if find_states([8]): #if dead
-            dead_x_velocity = self.nodes[: ,2]= 0
-            dead_y_velocity = self.nodes[:, 3] = 0
-            return self.dead
-
-        if find_states([7]): #if vaccinated
-            #Do something but don't interact if collision
-            return self.vaccinated
-
-        if find_states([6]): #if immune
-            #Pause collision contagion for given amount of time
-            self.immune 
-            time.sleep(5)
-            return self.immune
-
-        if not find_states([4]): #if healthy
-            if find_states([5]): #wearing a mask
-                return self.healthy_color_mask
-            else:  #no mask
-                return self.healthy_color_no_mask
-        elif find_states([4]): #if sick
-            if find_states([5]): #wearing a mask
-                return self.sick_color_mask
-            else: #no mask
-                return self.sick_color_no_mask
 
 
     def collision_detection(self):
