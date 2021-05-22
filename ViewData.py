4e827fdf0ab6006e0d8d7f2a9e1fb00e2659700f
bautzon <60885700+bautzon@users.noreply.github.com>
2021-05-22 13:01:20 +0200

    shiiz

---
+++ ViewData.py
@@ -0,0 +1,111 @@
+from tkinter import Label
+import numpy as np
+import matplotlib.pyplot as plt
+import main
+# from nodeSystem import NodeSystem as ns
+import matplotlib.animation as animation
+
+
+class AnimatedScatter(object):
+    """An animated scatter plot using matplotlib.animations.FuncAnimation."""
+    def __init__(self, data, pop_size):
+        self.fig, self.ax = plt.subplots()
+        self.data = data
+        self.pop_size=pop_size
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval = 5,
+                                           init_func=self.setup_plot, repeat = False, frames = self.data.shape[0], blit=True)
+        self.colors = ["red", "green", "black", "blue", "purple", "pink"]
+
+    def setup_plot(self):
+        x, y = self.data[0, :, 0], self.data[0, :, 1]
+        # self.scat = self.ax.scatter(x, y, cmap="jet", edgecolor="k", c="red")
+
+        self.scat = self.ax.scatter(x, y, edgecolors="none", color="red")
+
+        return self.scat,
+
+
+    def update(self, i):
+        """Update the scatter plot."""
+        data = self.data[i, :, :]
+        # Set x and y data...
+        self.scat.set_offsets(data[:, :2])
+
+        sizes = np.ones(data.shape[0])
+        self.scat.set_sizes(sizes + 30)
+        self.scat.set_array(data[:, 2])
+        
+        """
+        if np.any(self.data[:, 4] == 1):
+            self.scat.set_color("red")
+        """
+        
+        return self.scat,
+        #self.ax.text(1, 1, "Deathcount: " + sum(self.data[:,:,8]))
+
+def plot_nodes(pop, iterations):
+
+    with open("simlog.npy", "rb") as f:
+        data = np.load(f)
+
+
+    ac = AnimatedScatter(data, pop)
+    plt.show()
+
+def show_graph(iterations):
+    with open("simlog.npy", "rb") as f:
+        data = np.load(f)
+    # print(data[:, :, 4].sum())
+    gwaf = data.sum(axis = 1)
+    plt.plot(gwaf[:,4], color="red")
+    plt.plot(gwaf[:, 8], color="black")
+    plt.plot(gwaf[:, 6], color="magenta")
+    plt.title("Not a covid graph")
+    plt.xlabel("Days")
+    plt.ylabel("Number of People Affected")
+    plt.legend(["Infected", "Dead", "Immune"], loc="lower right")
+    plt.ylim([0,100])
+    # print(gwaf)
+    # plt.plot(gwaf, iterations)
+    plt.show()
+
+class Cursor:
+"""
+A cross hair cursor.
+"""
+def __init__(self, ax):
+    self.ax = ax
+    self.horizontal_line = ax.axhline(color='k', lw=0.8, ls='--')
+    self.vertical_line = ax.axvline(color='k', lw=0.8, ls='--')
+    # text location in axes coordinates
+    self.text = ax.text(0.72, 0.9, '', transform=ax.transAxes)
+
+def set_cross_hair_visible(self, visible):
+    need_redraw = self.horizontal_line.get_visible() != visible
+    self.horizontal_line.set_visible(visible)
+    self.vertical_line.set_visible(visible)
+    self.text.set_visible(visible)
+    return need_redraw
+
+def on_mouse_move(self, event):
+    if not event.inaxes:
+        need_redraw = self.set_cross_hair_visible(False)
+        if need_redraw:
+            self.ax.figure.canvas.draw()
+    else:
+        self.set_cross_hair_visible(True)
+        x, y = event.xdata, event.ydata
+        # update the line positions
+        self.horizontal_line.set_ydata(y)
+        self.vertical_line.set_xdata(x)
+        self.text.set_text('x=%1.2f, y=%1.2f' % (x, y))
+        self.ax.figure.canvas.draw()
+
+x = np.arange(0, 1, 0.01)
+y = np.sin(2 * 2 * np.pi * x)
+
+fig, ax = plt.subplots()
+ax.set_title('Simple cursor')
+ax.plot(x, y, 'o')
+cursor = Cursor(ax)
+fig.canvas.mpl_connect('motion_notify_event', cursor.on_mouse_move)
---
+++ interface.py
@@ -0,0 +1,79 @@
+import main, ViewData
+import tkinter as tk
+import sys
+
+
+def run():
+    runsim_button.configure(text="Running Simulation...")
+    main.runSim(population_slider.get(), iteration_slider.get(), masks_slider.get(), vaccer_slider.get(), mortality_rate_as_float)
+    runsim_button.configure(text="Complete Simulation")
+    view_results_graph_button.grid(row=8, column=0)
+    view_results_scatter_button.grid(row=9, column=0)
+
+def view_results_graph():
+
+    ViewData.show_graph(iteration_slider.get())
+
+def view_results_scatter():
+
+    ViewData.plot_nodes(population_slider.get(), iteration_slider.get())
+    
+#Instantiate root window
+root = tk.Tk()
+
+welcomemsg = tk.Label(root, text="Greetings from the PandemicSim dev team! \n Select a population size")
+welcomemsg.grid(row=0, column=0)
+
+population_slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, label="Population:", length=220, resolution=10)
+population_slider.grid(row=1, column=0)
+population_slider.set(100)
+
+masks_slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, label="Number of people with masks ( IN %):", length=220, resolution=5)
+masks_slider.grid(row=2, column=0)
+masks_slider.set(20)
+
+vaccer_slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, label="Number of people vaccinated ( IN %):", length=220, resolution=5)
+vaccer_slider.grid(row=3, column=0)
+vaccer_slider.set(20)
+
+mortality_msg = tk.Label(root, text="Insert mortality rate")
+mortality_msg.grid(row=4, column=0)
+mortality_textfield = tk.Entry(root,)
+mortality_textfield.grid(row=5, column=0)
+mortality_textfield.insert(0, "2.7")
+mortality_rate_as_float = float(mortality_textfield.get())
+
+iteration_slider = tk.Scale(root, from_=0, to=10000, orient=tk.HORIZONTAL, label="Iterations:", length=220, resolution=10)
+iteration_slider.grid(row=6, column=0)
+iteration_slider.set(4000)
+
+runsim_button = tk.Button(root, text="Run Simulation", bg="#82ff63", command=run)
+runsim_button.grid(row=7, column=0)
+
+view_results_graph_button = tk.Button(root, text="View Results", bg="#0096FF", command=view_results_graph)
+
+view_results_scatter_button = tk.Button(root, text="View Scatter Plot", bg="#0096FF", command=view_results_scatter)
+
+exit_button = tk.Button(root, text="Exit Application", bg="#FF0000",command= lambda: sys.exit())
+exit_button.grid(row=10, column=0)
+
+
+
+"""Helper functions to parse arguments to parameters.py"""
+def get_population():
+    return population_slider.get()
+
+def get_vaccinated():
+    return vaccer_slider.get()
+
+def get_masks():
+    return masks_slider.get()
+
+def get_iterations():
+    return iteration_slider.get()
+
+def get_mortality_rate():
+    return mortality_textfield.get()
+
+#Run loop of root window
+root.mainloop()
