654cf014165b95c0411b8feeefbedded52cc3ee1
Byttemos <hriskaer@gmail.com>
2021-05-22 09:54:34 +0200

    Init commit

---
+++ parameters.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+class Parameters:
+    def __init__(self, n, mask_procent, vac_procent, mortality_rate):
+       self.mask_procent = mask_procent
+       self.vac_procent = vac_procent
+       self.mortality_rate = mortality_rate
